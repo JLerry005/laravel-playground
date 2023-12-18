505d33299dce72b53bc9e6c6918b435aa2f84d01
Pinoy Builders <jlerry005@gmail.com>
2023-12-18 17:18:56 +0800

    let's begin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <div class="bg-red-500">
+        <div> w-96 </div>
+        <div> w-80 </div>
+        <div> w-64 </div>
+        <div> w-48 </div>
+        <div> w-40 </div>
+        <div> w-32 </div>
+        <div> w-24 </div>
+    </div>
+</body>
+</html>
